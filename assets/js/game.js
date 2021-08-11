17faed963b3a57885a7c190899363b5598e67728
cgar246 <christiang3268@yahoo.com>
2021-08-11 07:42:19 -0700

    adding array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+//game states
+// "WIN" - player robot has defeated all enemy-robots
+//  * fight all enemy-robots
+//  * defeat each enemy-robot
+// "LOSE - player robot's health is zero or less"
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,12 +12,22 @@ var playerMoney = 10;
 //you can also log multiple calues at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index ");
+}
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert('Welcome to Robot Gladiators');
 
@@ -66,4 +82,7 @@ var fight = function() {
   }
 };
 
-fight()
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i])
+}
+// fight()
