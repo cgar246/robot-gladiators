2ba96927a02aa357fa14c7bdcffd6c40d09a9b76
cgar246 <christiang3268@yahoo.com>
2021-08-13 15:43:07 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,51 +1,56 @@
-//game states
-// "WIN" - player robot has defeated all enemy-robots
-//  * fight all enemy-robots
-//  * defeat each enemy-robot
-// "LOSE - player robot's health is zero or less"
-
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-//you can also log multiple calues at once like this
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames.length);
-for(var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index ");
-}
-
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// fight function
-var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert('Welcome to Robot Gladiators');
+console.log(enemyNames);
+console.log(enemyNames.lenght);
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
 
+// fight function (with parameter for enemy's name)
+var fight = function(enemyName) {
+    while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose");
 
-    // if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
+    // if player choses to skip
+    if (promptFight === "skip" || promptFight === "SKIP") {
+        // confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+         // if yes (true), leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight. Goodbye!");
+        // subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 10;
+        console.log("playerMoney", playerMoney);
+        break;
+    }
+}
+
+
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
         );
   
-    // check enemy's health
-    if (enemyHealth <= 0) {
+        // check enemy's health
+        if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-    } else {
+
+        // award player money for winning
+        playerMoney = playerMoney +20
+
+        // leave while() loop since enemy is dead
+        break;
+        } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
   
@@ -58,31 +63,69 @@ var fight = function(enemyName) {
     // check player's health
     if (playerHealth <= 0) {
       window.alert(playerName + " has died!");
+      // leave while() loop if player is dead
+      break;
     } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-    // if player choses to skip
-} else if (promptFight === "skip" || promptFight === "SKIP") {
-    // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-  // if yes (true), leave fight
-  if (confirmSkip) {
-    window.alert(playerName + " has decided to skip this fight. Goodbye!");
-    // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 2;
-  }
-  // if no (false), ask question again by running fight() again
-  else {
-    fight();
-  }
-
-  } else {
-    window.alert("You need to choose a valid option. Try again!");
-  }
-};
-
-for(var i = 0; i < enemyNames.length; i++){
-    fight(enemyNames[i])
 }
-// fight()
+}
+
+// function to end the entire game
+    var endGame = function(){
+        window.alert("The game has now ended. Lets see how you did!");
+        // if player is still alive, player wins!
+        if (player > 0) {
+            window.alert("Great Job, you've survived the game! You now have a score of " + playerMoney + ".");
+        } else {
+            window.alert("You've lost your robot in battle.")
+        }
+        var playAgainConfirm = window.confirm("Would you like to play again?");
+        
+        if (playerAgainConfirm) {
+            //restart game
+            startGame();
+        } else {
+            window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+        }
+    }
+
+    // function to start a new game
+var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+// fight each enemy-robot by looping over them and fighting them one at a time
+for (var i = 0; i < enemyNames.length; i++) {
+    // if player is still alive, keep fighting
+    if (playerHealth>0) {
+        //let player know what round they are in,
+        window.alert('Welcome to Robot Gladiators! Round ' + (i+1));
+
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+
+        // use debugger to pause script form running and check what's going on at that moment in the code
+        // debugger;
+
+        //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight(pickedEnemyName);
+    }
+    // if player isn't alive; stop the game
+    else {
+        window.alert('You have lost your robot in battle! Game Over!');
+        break;
+    }
+}
+// after the loop ends, player is either out of health of enemies to fight, so run the endGame function
+endGame();
+// start the game when the page loads
+//startGame();
+}
+
+
